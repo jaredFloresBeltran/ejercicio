fac6d4b635bc6ff7792ba2b6be4479c50bd7bc1c
jared <20243ds034@utez.edu.mx>
2025-05-21 08:45:29 -0600

    Main.java

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String [] args) {
+        Double num1= Double.valueOf(JOptionPane.showInputDialog("Dame el primer numero"));
+        Double num2= Double.valueOf(JOptionPane.showInputDialog("Dame el segundo numero"));
+        Double num3= Double.valueOf(JOptionPane.showInputDialog("Dame el tercer numero"));
+
+        System.out.println((Double.valueOf(num1) + Double.valueOf(num2) + Double.valueOf(num3))/3);
+
+    }
+
+
+
+}
